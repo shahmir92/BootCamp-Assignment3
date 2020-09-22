b48fa9269067d5a2a746758bef8cb0d10d13f037
shahmir92 <shahmir375@gmail.com>
2020-09-22 14:11:30 +0500

    Small tweaks in Standars App with css features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+  return <div className = "bg">
+      Hello World from App.js <br></br>
+      Greetings from {props.name}
+      and my age is {2020 - props.age}
     </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className = "bgi" >
+      Okay Hello world from Index.js
+      <App name="Shahmir " age = {1997} />
+      </div>,
   document.getElementById('root')
 );
 
